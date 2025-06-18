824dc9de46eecfb9a385121a36b5475e798a9f12
zq405 <77684730+zq405@users.noreply.github.com>
2025-06-19 02:20:58 +0800

    Update Retail_Outlet.cpp

--- Retail_Outlet.cpp
+++ Retail_Outlet.cpp
@@ -10,45 +10,121 @@ class Product
     public:
         Product():name(""),price(0.0){}
         Product (string n,double p):name(n),price(p){}
-
-        Product(const Product& other): name(other.name),price(other.price){}
         ~Product() {}
         virtual void display() 
         {
             cout << "Item: " << name << endl << "Price:RM " << price << endl;
         }
-        friend double calTotal(const Product& item);
+        friend double calTotal(Product& item);
 };
 
 class FoodItem :public Product 
 {
     private:
-        string cuisine;
+        string expiryDate;
     public:
-        FoodItem():cuisine(""){}
-        FoodItem(string n,double p,string c):Product(n,p),cuisine(c){}
-        FoodItem(const FoodItem& other): Product(other),cuisine(other.cuisine){}
+        FoodItem():Product(),expiryDate(""){}
+        FoodItem(string n,double p,string e):Product(n,p),expiryDate(e){}
         ~FoodItem(){}
-        void display()const 
+        void display() override 
         {
-            cout << "Food Item : " << name << endl << "Price :RM" << price << endl << "Cuisine: " << cuisine << endl;
+            Product::display();
+            cout<<"Expiry Date : "<<expiryDate<<endl;
         }
 };
 
 class BeverageItem :public Product 
 {
     private:
-        bool isAlcoholic;
+        string isAlcohol;
     public:
-        BeverageItem():isAlcoholic(""){}
-        BeverageItem(string n,double p,bool a):Product(n,p),isAlcoholic(a){}
-        BeverageItem(const BeverageItem& other): Product(other),isAlcoholic(other.isAlcoholic){}
+        BeverageItem():Product(),isAlcohol("Y/N"){}
+        BeverageItem(string n,double p,string a):Product(n,p),isAlcohol(a){}
         ~BeverageItem(){}
         void display() override
         {
-            cout<<"Beverage Item : "<<name<<endl;
-            cout<<"Price : RM "<<price<<endl;
-            cout<<
+            Product::display();
+            cout<<"Does it contain alcohol? : "<<isAlcohol<<endl;
         }
 
 };
+
+
+void display_menu();
+void add(Product *products,int &count);
+void edit(Product *products,int count);
+void deleteProduct(Product *products, int &count);
+void displayProduct(Product *products, int &count);
+
+
+int main()
+{
+    int choice;
+    int count=0;
+    Product *products=nullptr;
+
+    do
+    {
+        display_menu();
+        cin>>choice;
+
+    }while (choice!=5);
+}
+
+void display_menu()
+{
+    cout<<"\n=========Reatil Outlet Menu========="<<endl;
+    cout<<"\n1. Add Product";
+    cout<<"\n2. Edit Product discount";
+    cout<<"\n3. Delete Product";
+    cout<<"\n4. Display Product";
+    cout<<"\n5. Exit";
+}
+
+void add(Product **products,int &count)
+{
+    int type;
+    string name;
+    double price;
+    cout<<"\nPlease choose the type of item that you want to add\n";
+    cout<<"1.Food Item\n";
+    cout<<"2.Beverage Item\n";
+    cin>>type;
+    cin.ignore();
+    cout<<"Enter Item name : ";
+    getline(cin,name);
+    cout<<"Enter Item Price : RM";
+    cin>>price;
+
+    Product *newProduct=nullptr;
+    if(type==1)
+    {
+        string expiry_date;
+        cout<<"Enter Item expiry date : ";
+        getline(cin,expiry_date);
+        newProduct=new FoodItem(name,price,expiry_date);
+        cin.ignore();
+    }
+    else if(type==2)
+    {
+        string alcohol;
+        cout<<"Is it contain alcohol? : ";
+        getline(cin,alcohol);
+        newProduct=new BeverageItem(name,price,alcohol);
+    }
+
+    Product **temporary=new Product*[count+1];
+    for(int i=0;i<count;i++)
+    {
+        temporary[i]=products[i];
+        temporary[count]=newProduct;
+        delete[] products;
+        products= temporary;
+        count++;
+    }
+}
+
+void edit(Product *products,int count)
+{
+
+}
